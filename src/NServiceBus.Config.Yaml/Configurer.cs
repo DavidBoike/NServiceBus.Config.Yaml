a3014a876aff4057b12b4644dc488142591ddc12
David Boike <david.boike@gmail.com>
2016-08-06 00:01:21 -0500

    Very naive ability to set error queue, assuming YAML value would be string

---
+++ src/NServiceBus.Config.Yaml/Configurer.cs
@@ -0,0 +1,107 @@
+﻿namespace NServiceBus.Config.Yaml
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Reflection;
+    using System.Runtime.CompilerServices;
+    using YamlDotNet.RepresentationModel;
+
+    class Configurer
+    {
+        EndpointConfiguration cfg;
+        Dictionary<Type, Dictionary<string, MethodInfo>> extensionMethods;
+
+        public Configurer(EndpointConfiguration cfg)
+        {
+            this.cfg = cfg;
+
+            extensionMethods = new Dictionary<Type, Dictionary<string, MethodInfo>>();
+        }
+
+        public void ApplyYaml(TextReader reader)
+        {
+            using (reader)
+            {
+                var yaml = new YamlStream();
+                yaml.Load(reader);
+                Apply(cfg, yaml);
+            }
+        }
+
+        void Apply(EndpointConfiguration cfg, YamlStream yaml)
+        {
+            var yamlRoot = (YamlMappingNode)yaml.Documents[0].RootNode;
+            foreach (var pair in yamlRoot.Children)
+            {
+                if (pair.Key.NodeType == YamlNodeType.Scalar)
+                {
+                    var keyNode = (YamlScalarNode)pair.Key;
+                    var key = keyNode.Value;
+                    Apply(cfg, key, pair.Value);
+                }
+                else
+                {
+                    throw new Exception("Don't know what to do when key isn't a scalar...");
+                }
+            }
+        }
+
+        void Apply(EndpointConfiguration cfg, string key, YamlNode value)
+        {
+            var method = GetExtensionMethod(typeof(EndpointConfiguration), key);
+            if (method != null)
+            {
+                var parameters = method.GetParameters();
+                var paramValues = new object[parameters.Length];
+                paramValues[0] = cfg;
+                paramValues[1] = ((YamlScalarNode) value).Value;
+                method.Invoke(null, paramValues);
+            }
+            else
+            {
+                throw new Exception("Couldn't find an extension method");
+            }
+        }
+
+        MethodInfo GetExtensionMethod(Type target, string methodName)
+        {
+            Dictionary<string, MethodInfo> methods;
+            if (!extensionMethods.TryGetValue(target, out methods))
+            {
+                methods = new Dictionary<string, MethodInfo>();
+
+                foreach (var assem in AppDomain.CurrentDomain.GetAssemblies())
+                {
+                    foreach (var type in assem.GetTypes())
+                    {
+                        if (type.IsSealed && !type.IsGenericType && !type.IsNested && type.IsDefined(typeof(ExtensionAttribute), false))
+                        {
+                            foreach (var method in type.GetMethods(BindingFlags.Static | BindingFlags.Public))
+                            {
+                                if (method.IsDefined(typeof(ExtensionAttribute), false))
+                                {
+                                    var parameters = method.GetParameters();
+                                    if (parameters[0].ParameterType == target)
+                                    {
+                                        methods[method.Name] = method;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+
+                extensionMethods[target] = methods;
+            }
+
+            MethodInfo lookupMethod;
+            if (methods.TryGetValue(methodName, out lookupMethod))
+            {
+                return lookupMethod;
+            }
+    
+            return null;
+        }
+    }
+}
--- src/NServiceBus.Config.Yaml/EndpointConfigurationExtensions.cs
+++ src/NServiceBus.Config.Yaml/EndpointConfigurationExtensions.cs
@@ -1,33 +1,19 @@
 ﻿namespace NServiceBus.Config.Yaml
 {
     using System.IO;
-    using YamlDotNet.RepresentationModel;
 
     public static class EndpointConfigurationExtensions
     {
         public static void FromYaml(this EndpointConfiguration cfg, string path)
         {
-            using (var reader = new StreamReader(path))
-            {
-                var yaml = new YamlStream();
-                yaml.Load(reader);
-                Apply(cfg, yaml);
-            }
+            var worker = new Configurer(cfg);
+            worker.ApplyYaml(new StreamReader(path));
         }
 
         public static void FromYamlString(this EndpointConfiguration cfg, string yamlString)
         {
-            using (var reader = new StringReader(yamlString))
-            {
-                var yaml = new YamlStream();
-                yaml.Load(reader);
-                Apply(cfg, yaml);
-            }
-        }
-
-        static void Apply(EndpointConfiguration cfg, YamlStream yaml)
-        {
-            
+            var worker = new Configurer(cfg);
+            worker.ApplyYaml(new StringReader(yamlString));
         }
     }
 }
